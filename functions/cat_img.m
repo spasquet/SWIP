83db934ac72d4b8146869faab145c6286ac7bfc8
lbdt <ludovic.bodet@upmc.fr>
2016-08-24 17:50:38 +0200

    Update cat_img.m

--- functions/cat_img.m
+++ functions/cat_img.m
@@ -3,6 +3,7 @@ function cat_img(infiles,imgformat,columns,align,outfile,verbose)
 % Concatenate images files in various format
 %
 % cat_img(infiles,imgformat,columns,outfile)
+%....
 
 if exist('infiles','var')==0 || isempty(infiles)==1
     infiles=[];
